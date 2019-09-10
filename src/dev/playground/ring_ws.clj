59ed63ab55325166de04af1c2973a44a4bc9b69a
Matus Kmit <matus.kmit@zhdk.ch>
2019-09-10 14:07:36 +0200

    subs vars

--- src/dev/playground/ring_ws.clj
+++ src/dev/playground/ring_ws.clj
@@ -15,11 +15,11 @@
             [clojure.tools.logging :as log])
   (:import (clojure.lang ExceptionInfo)))
 
-(defn subscription [channel id query-string]
+(defn subscription [channel id {:keys [query variables]} request]
   (let [[prepared-query errors] (try
                                   [(-> schema
-                                       (parser/parse-query query-string)
-                                       (parser/prepare-with-query-variables {:msg "test"}))]
+                                       (parser/parse-query query)
+                                       (parser/prepare-with-query-variables variables))]
                                   (catch ExceptionInfo e
                                     [nil e]))
 
@@ -30,9 +30,10 @@
                                 (send! channel)))]
     (if (some? errors)
       (->> errors ex-data str (send! channel))
-      (let [cleanup-fn (executor/invoke-streamer
-                         {constants/parsed-query-key prepared-query}
-                         source-stream-fn)]
+      (let [cleanup-fn
+            (executor/invoke-streamer
+              {constants/parsed-query-key prepared-query, :request request}
+              source-stream-fn)]
         (println "call cleanup-fn return" (cleanup-fn))))))
 
 (defn graphql-ws-handler [request]
@@ -41,20 +42,18 @@
     (println "New WebSocket channel:" channel)
     (on-receive channel
                 (fn [data]
-                  (let [{:keys [id payload type] :as data-map} (json/parse-string data keyword)]
-                    (log/info data-map)
+                  (let [{:keys [id payload type]}
+                        (json/parse-string data keyword)]
                     (case type
-                      "connection_init"
-                      (send! channel (json/generate-string {:type "connection_ack"}))
+                      "connection_init" (->> {:type "connection_ack"}
+                                             json/generate-string
+                                             (send! channel))
 
-                      "start"
-                      (subscription channel id (:query payload))
+                      "start" (subscription channel id payload request)
 
-                      "stop"
-                      (prn "todo")
+                      "stop" (prn "todo")
 
-                      "connection_terminate"
-                      (prn :todo)
+                      "connection_terminate" (prn :todo)
 
                       ;; Not recognized!
                       (prn type)))))
