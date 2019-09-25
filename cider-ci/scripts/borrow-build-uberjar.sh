12bb09da0f449ef59b507365584c49970d989416
Matus Kmit <matus.kmit@zhdk.ch>
2019-09-25 16:22:18 +0200

    cider-ci

---
+++ cider-ci/scripts/borrow-build-uberjar.sh
@@ -0,0 +1,10 @@
+export PROJECT_DIR="$LEIHS_BORROW_DIR"
+export UBERJAR_NAME="leihs-borrow"
+LEIN_UBERJAR_PATH="${PROJECT_DIR}/target/${UBERJAR_NAME}.jar" # path of the expeced repository uberjar path
+
+function build_uberjar() {
+  echo "INFO: building the ${UBERJAR_NAME} uberjar now"
+  cd $LEIHS_BORROW_DIR
+  export LEIN_SNAPSHOTS_IN_RELEASE=yes
+  boot uberjar
+}
