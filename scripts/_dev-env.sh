ab06e829822b3bf93d6e9a91ceb38c9cd929810e
Max Albrecht <max.albrecht@zhdk.ch>
2019-10-31 14:32:07 +0100

    dev scripts

--- scripts/_dev-env.sh
+++ scripts/_dev-env.sh
@@ -1,2 +1,5 @@
-export LEIHS_DATABASE_URL="jdbc:postgresql://localhost:5432/leihs_prod?max-pool-size=5"
+export DATABASE_URL="postgresql://localhost:5432/leihs_prod?max-pool-size=5" 
+export LEIHS_DATABASE_URL="jdbc:${DATABASE_URL}" 
+
 export LEIHS_LEGACY_PORT=3000
+export LEIHS_LEGACY_HTTP_BASE_URL="http://localhost:${LEIHS_LEGACY_PORT}"
