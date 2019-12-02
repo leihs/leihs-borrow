01da6b14bf2f96e2ef3375a56fe3bd3fdb091a1b
Max Albrecht <max.albrecht@zhdk.ch>
2019-12-02 12:21:45 +0100

    html: add tailwind css from npm package

--- src/all/leihs/borrow/html.clj
+++ src/all/leihs/borrow/html.clj
@@ -13,9 +13,7 @@
    [:meta {:name "viewport"
            :content "width=device-width, initial-scale=1, shrink-to-fit=no"}]
 
-   ; TMP add precompiled tailwindcss
-   [:link {:rel "stylesheet" :href "https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"}]
-   
+   (hiccup.page/include-css (cache-buster/cache-busted-path "/borrow/css/tailwind.min.css"))
    (hiccup.page/include-css (cache-buster/cache-busted-path "/borrow/css/base-styles.css"))])
 
 (defn not-found-handler [_request]
