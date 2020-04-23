118ee6e1d445065ce99bdb23a894c635d447f482
Matus Kmit <matus.kmit@zhdk.ch>
2020-04-23 12:02:55 +0200

    just timestamp

--- src/all/leihs/borrow/client/ui/main_nav.cljs
+++ src/all/leihs/borrow/client/ui/main_nav.cljs
@@ -11,8 +11,6 @@
 
 (defn navbar []
   (let [current-order @(rf/subscribe [:leihs.borrow.client.features.shopping-cart.core/current-order])
-        valid-until (some->> (:valid-until current-order) js/Date.)
-        timeout? (some->> valid-until (datefn/isAfter (js/Date.)))
 
         style {:top 0 :z-index 1000
                :border-bottom-color "rgba(0,0,0, 0.08)"
@@ -24,12 +22,10 @@
                  icons/menu-icon]
         brand [:a.navbar-brand.m-0.font-black.text-xl {:href (routing/path-for ::routes/home)}
                (str " " "LEIHS" " ")]
-        timer (when valid-until
-                (if timeout?
-                  [:span {:class "text-color-danger"} "!!!"]
-                  [:span {:class "text-color-info"} (datefn/formatDistanceToNow (js/Date. valid-until))]))
+        valid-until [:span {:class "text-color-info"} (some->> current-order :data :valid-until)]
         cart [:a.nav-item.nav-link.px-0 {:href (routing/path-for ::routes/shopping-cart)}
               icons/shopping-cart-icon]]
+    (js/console.log valid-until)
 
     [:nav.ui-main-nav.navbar.navbar-light.text-xl.shadow-md.py-0.px-2.sticky-top.flex-nowrap.justify-content-between
      {:style style}
@@ -41,6 +37,6 @@
 
      [:div.navbar-nav.w-100
       [:div.ml-auto.d-flex.align-items-center
-       [:div.mx-auto.px-2.text-xs timer]
+       [:div.mx-auto.px-2.text-xs valid-until]
        " " cart]]]))
 
