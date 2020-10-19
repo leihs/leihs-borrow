037b2f87457a5fe87fe94cc06d1b2f3e2f006da8
Matus Kmit <matus.kmit@zhdk.ch>
2020-10-19 14:30:01 +0200

    remove id after http complete

--- src/client/leihs/borrow/lib/requests.cljs
+++ src/client/leihs/borrow/lib/requests.cljs
@@ -1,6 +1,7 @@
 (ns leihs.borrow.lib.requests
   (:require [re-graph.core :as re-graph]
             [re-frame.core :as rf]
+            [leihs.core.core :refer [flip]]
             [leihs.borrow.lib.helpers :refer [spy log]]
             [leihs.borrow.lib.re-frame :refer [reg-event-fx
                                                reg-event-db
@@ -24,16 +25,17 @@
       {:dispatch-n events})))
 
 (rf/reg-global-interceptor
-  (rf/->interceptor :after
-                    (fn [ctx]
-                      (let [[ev-id :as ev] (-> ctx :coeffects :event)]
-                        (if (= ev-id ::re-graph/mutate)
-                          (update-in ctx
-                                     [:effects :db ::running-mutations-ids]
-                                     conj
-                                     (-> ctx
-                                         :effects
-                                         :re-graph.internals/send-http
-                                         spy
-                                         (nth 1)))
-                          ctx)))))
+  (rf/->interceptor
+    :id :track-mutation-ids
+    :after (fn [ctx]
+             (let [[ev-id :as ev] (-> ctx :coeffects :event)
+                   path [:effects :db ::running-mutations-ids]]
+               (case ev-id
+                 ::re-graph/mutate
+                 (update-in ctx path conj (-> ctx
+                                              :effects
+                                              :re-graph.internals/send-http
+                                              (nth 1)))
+                 :re-graph.internals/http-complete
+                 (update-in ctx path (flip remove) #{(nth ev 2)})
+                 ctx)))))
