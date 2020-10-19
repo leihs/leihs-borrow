8923972bbf99d91bbe81c8fb505fa66391a56b51
Matus Kmit <matus.kmit@zhdk.ch>
2020-10-19 13:44:32 +0200

    running mutations ids

--- src/client/leihs/borrow/lib/requests.cljs
+++ src/client/leihs/borrow/lib/requests.cljs
@@ -1,6 +1,7 @@
 (ns leihs.borrow.lib.requests
   (:require [re-graph.core :as re-graph]
-            [leihs.borrow.lib.helpers :refer [spy]]
+            [re-frame.core :as rf]
+            [leihs.borrow.lib.helpers :refer [spy log]]
             [leihs.borrow.lib.re-frame :refer [reg-event-fx
                                                reg-event-db
                                                reg-sub
@@ -21,3 +22,18 @@
           events (->> req-ids
                       (map (partial vector ::re-graph/abort)))]
       {:dispatch-n events})))
+
+(rf/reg-global-interceptor
+  (rf/->interceptor :after
+                    (fn [ctx]
+                      (let [[ev-id :as ev] (-> ctx :coeffects :event)]
+                        (if (= ev-id ::re-graph/mutate)
+                          (update-in ctx
+                                     [:effects :db ::running-mutations-ids]
+                                     conj
+                                     (-> ctx
+                                         :effects
+                                         :re-graph.internals/send-http
+                                         spy
+                                         (nth 1)))
+                          ctx)))))
