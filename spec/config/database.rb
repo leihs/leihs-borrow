26d06ca5ae72e25e53ba376fe61c5d8efaa853f5
Max Albrecht <max.albrecht@zhdk.ch>
2021-08-04 15:31:36 +0200

    spec: config: restore translations quietly

--- spec/config/database.rb
+++ spec/config/database.rb
@@ -5,20 +5,41 @@ DB_ENV = ENV['LEIHS_DATABASE_URL'].presence
 
 def http_uri
   # trick Addressable to parse db urls
-  @http_uri ||= \
-    Addressable::URI.parse DB_ENV.gsub(/^jdbc:postgresql/,'http').gsub(/^postgres/,'http')
+  @http_uri ||=
+    Addressable::URI.parse DB_ENV.gsub(/^jdbc:postgresql/, 'http').gsub(
+                             /^postgres/,
+                             'http'
+                           )
 end
 
 def database
-  @database ||= \
+  @database ||=
     begin
-      db_url = 'postgres://' \
-        + (http_uri.user.presence || ENV['PGUSER'].presence || 'postgres') \
-        + ((pw = (http_uri.password.presence || ENV['PGPASSWORD'].presence)) ? ":#{pw}" : "") \
-        + '@' + (http_uri.host.presence || ENV['PGHOST'].presence || ENV['PGHOSTADDR'].presence || 'localhost') \
-        + ':' + (http_uri.port.presence || ENV['PGPORT'].presence || 5432).to_s \
-        + '/' + ( http_uri.path.presence.try(:gsub,/^\//,'') || ENV['PGDATABASE'].presence || 'leihs_test') \
-        + '?pool=5'
+      db_url =
+        'postgres://' +
+          (http_uri.user.presence || ENV['PGUSER'].presence || 'postgres') +
+          (
+            if (pw = (http_uri.password.presence || ENV['PGPASSWORD'].presence))
+              ":#{pw}"
+            else
+              ''
+            end
+          ) +
+          '@' +
+          (
+            http_uri.host.presence || ENV['PGHOST'].presence ||
+              ENV['PGHOSTADDR'].presence ||
+              'localhost'
+          ) +
+          ':' +
+          (http_uri.port.presence || ENV['PGPORT'].presence || 5_432).to_s +
+          '/' +
+          (
+            http_uri.path.presence.try(:gsub, %r{^\/}, '') ||
+              ENV['PGDATABASE'].presence ||
+              'leihs_test'
+          ) +
+          '?pool=5'
 
       Sequel.connect(db_url)
     end
@@ -40,18 +61,19 @@ def clean_db
     ORDER BY table_type, table_name;
   SQL
 
-  database[sql]
-    .map { |r| r[:table_name] }
-    .reject { |tn| ['schema_migrations', 'translations_default'].include?(tn) }
-    .join(', ')
+  database[sql].map { |r| r[:table_name] }.reject do |tn|
+    %w[schema_migrations translations_default].include?(tn)
+  end.join(', ')
     .tap { |tables| database.run " TRUNCATE TABLE #{tables} CASCADE; " }
 end
 
 RSpec.configure do |config|
-  config.before(:example)  do
+  config.before(:example) do
     clean_db
-    system("DATABASE_NAME=#{http_uri.basename} ./database/scripts/restore-seeds")
-    system("bin/get-translations | psql -d #{http_uri.basename}")
+    system(
+      "DATABASE_NAME=#{http_uri.basename} ./database/scripts/restore-seeds"
+    )
+    system("bin/get-translations | psql --quiet -d #{http_uri.basename}")
   end
   # config.after(:suite) do
   #   clean_db
