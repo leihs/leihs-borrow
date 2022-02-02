87e143e72a91afab8d550a8ab3eec2a5efe40ffc
Thomas Schank <DrTom@schank.ch>
2022-02-02 08:57:14 +0100

    REBASE START Upgrade build system, upgrades deps, CI config clean up

--- src/server/leihs/borrow/resources/templates.clj
+++ src/server/leihs/borrow/resources/templates.clj
@@ -89,8 +89,6 @@
       first))
 
 ;#### debug ###################################################################
-; (logging-config/set-logger! :level :debug)
-; (logging-config/set-logger! :level :info)
 ; (debug/debug-ns 'cider-ci.utils.shutdown)
 ; (debug/debug-ns *ns*)
 ; (debug/undebug-ns *ns*)
--- src/server/leihs/borrow/ssr.clj
+++ src/server/leihs/borrow/ssr.clj
@@ -1,18 +1,17 @@
 (ns leihs.borrow.ssr
-  (:require [hiccup.page :refer [html5 include-js]]
-            [leihs.core.shared :refer [head]]
-            [leihs.core.http-cache-buster2 :as cache-buster]))
+  (:require
+    [hiccup.page :refer [html5 include-js]]
+    [leihs.core.http-cache-buster2 :as cache-buster]
+    [leihs.core.shared :refer [head]]
+    ))
 
-(intern
-  'leihs.core.ssr
-  'render-page-base
-  (fn [inner-html]
-    (html5
-      (head
-        (hiccup.page/include-css (cache-buster/cache-busted-path
-                                   "/app/borrow/css/theme/bootstrap-leihs.css")))
-      [:body {:class "bg-paper"}
-       [:noscript "This application requires Javascript."]
-       inner-html
-       (hiccup.page/include-js (cache-buster/cache-busted-path
-                                 "/app/borrow/leihs-shared-bundle.js"))])))
+(defn render-page-base [inner-html]
+  (html5
+    (head
+      (hiccup.page/include-css (cache-buster/cache-busted-path
+                                 "/app/borrow/css/theme/bootstrap-leihs.css")))
+    [:body {:class "bg-paper"}
+     [:noscript "This application requires Javascript."]
+     inner-html
+     (hiccup.page/include-js (cache-buster/cache-busted-path
+                               "/app/borrow/leihs-shared-bundle.js"))]))
