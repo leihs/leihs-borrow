a5b8eae41520668923cf33b9d7f34786e87f0ac9
Matus Kmit <matuskmit1@gmail.com>
2022-07-21 16:07:41 +0200

    send submitted mails

--- src/server/leihs/borrow/resources/orders.clj
+++ src/server/leihs/borrow/resources/orders.clj
@@ -380,7 +380,8 @@
               sql/format
               (rs/query tx))
           (swap! after-tx-hooks* conj (fn [_req _resp]
-                                        (mails/send-received context order)))))
+                                        (mails/send-received context order)
+                                        (mails/send-submitted context order)))))
       (get-one-by-id tx user-id uuid))))
 
 (defn cancel
