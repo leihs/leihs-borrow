d177134a7fe045fc9097c29495c519a4b800edb9
Matus Kmit <matuskmit1@gmail.com>
2022-07-21 16:07:41 +0200

    send submitted mails

--- src/server/leihs/borrow/mails.clj
+++ src/server/leihs/borrow/mails.clj
@@ -15,3 +15,16 @@
                     (:body response))))
       (catch Throwable e
         (ring-ex/log e)))))
+
+(defn send-submitted [context order]
+  (if (-> context :request :settings :deliver_received_order_notifications)
+    (try
+     (let [response (legacy/post "/mail/submitted" context {:order_id (:id order)})
+           status (:status response)]
+       (when-not (= status 202)
+         (log/warn "Legacy responded with status:"
+                   (str status ",")
+                   "and body:"
+                   (:body response))))
+     (catch Throwable e
+       (ring-ex/log e)))))
