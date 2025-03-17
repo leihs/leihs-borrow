ac6cc48bccaf706d843a01431d8b549a102f51a7
Matus Kmit <matuskmit1@gmail.com>
2025-03-17 16:03:03 +0100

    ruby 3.3.7
    refact script
    
    remove edn
    
    yml file

--- cider-ci/generators/bin/feature-tasks-create.rb
+++ cider-ci/generators/bin/feature-tasks-create.rb
@@ -4,26 +4,32 @@ require 'pry'
 require 'active_support/all'
 require 'json'
 
-PROJECT_DIR = Pathname.new(__FILE__).expand_path.join("../../../..")
-features_json = IO.read PROJECT_DIR.join('tmp/features.json')
-features = JSON.parse(features_json).with_indifferent_access 
+begin
+  PROJECT_DIR = Pathname.new(__FILE__).expand_path.join("../../../..")
+  features_json = IO.read PROJECT_DIR.join('tmp/features.json')
+  features = JSON.parse(features_json).with_indifferent_access 
 
-task_template = IO.read(
-  PROJECT_DIR.join("cider-ci", "generators", "feature-task-template.yml"))
+  task_template = IO.read(
+    PROJECT_DIR.join("cider-ci", "generators", "feature-task-template.yml")
+  )
 
-# name must be unique but they are not: append index
-$name_id = {}
-def set_name(example)
-  name = example[:full_description].remove(example[:description]).strip()
-  $name_id[name] = 1 + ($name_id[name] || 0)
-  example[:name] = name + " " + $name_id[name].to_s
-  example
-end
+  # name must be unique but they are not: append index
+  $name_id = {}
+  def set_name(example)
+    name = example[:full_description].remove(example[:description]).strip
+    $name_id[name] = 1 + ($name_id[name] || 0)
+    example[:name] = name + format(" %02d", $name_id[name])
+    example
+  end
 
-File.open(PROJECT_DIR.join("cider-ci", "generators", "feature-tasks.yml"), "w") do |f|
-  features[:examples].map{|example|
-    set_name(example)
-  }.each{  |example|
-    f.write (task_template % example.as_json.transform_keys(&:to_sym))
-  }
+  File.open(PROJECT_DIR.join("cider-ci", "generators", "feature-tasks.yml"), "w") do |f|
+    features[:examples].map { |example|
+      set_name(example)
+    }.each { |example|
+      f.write(task_template % example.as_json.transform_keys(&:to_sym))
+    }
+  end
+rescue => e
+  puts e
+  exit(-1)
 end
--- spec/graphql/graphql_helper.rb
+++ spec/graphql/graphql_helper.rb
@@ -1,4 +1,3 @@
-require "edn"
 require "faraday"
 require "rack"
 
